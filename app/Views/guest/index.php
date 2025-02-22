89582a527be5cabba9c58c0e3c94af86c00d73a1
ach.hibatillah <ach.hibatillah@gmail.com>
2025-02-22 20:37:09 +0700

    Update

--- app/Views/guest/index.php
+++ app/Views/guest/index.php
@@ -1,23 +1,25 @@
 <section class="bg-clr1">
     <div class="row m-0 p-0" style="min-height:100vh;">
-        <div class="col-md-6 m-0 p-0 d-flex justify-content-center align-items-center">
-            <div class="mx-5 mt-5">
-                <div class="mt-5 mt-md-3 mb-0 mb-md-5">.</div>
-                <h4 class="text-clr5 mt-5 mt-md-0">Hello Guys</h4>
-                <h3 class="text-clr4">We Are <i class="text-clr5 fst-normal">CodeMinds</i></h3>
-                <p class="text-clr5">Jasa Pembuatan Website dan Aplikasi</p>
-                <hr class="text-clr5" style="width:100px;">
-                <p class="text-clr5 text-justify">CodeMinds adalah tim profesional yang menyediakan layanan unggulan dalam pengembangan website, aplikasi, dan konsultasi IT. Kami juga berpengalaman dalam desain UI/UX untuk memastikan tampilan dan pengalaman pengguna yang optimal. Dengan pendekatan kolaboratif dan inovatif, kami siap membantu Anda mewujudkan ide menjadi solusi digital yang fungsional dan estetis.</p>
-                <a href="<?= base_url('') ?>" class="btn btn-clr4 mt-3"><i class="fas fa-phone me-2"></i> konsultasi dulu aja</a>
+        <div class="col-md-7 m-0 p-0 d-flex justify-content-center align-items-center">
+            <div class="mx-5">
+                <div class="mx-0 mx-md-3 mx-lg-5 mt-0 mt-md-5 mt-lg-0 pt-0 pt-md-5 pt-lg-0">
+                    <div class="mt-5 mt-md-3 mb-0 mb-md-5">.</div>
+                    <h4 class="text-clr5 mt-5 mt-md-0 mb-2">Hello Guys</h4>
+                    <h1 class="text-clr4 fw-bold mb-2">We Are <i class="text-clr5 fst-normal">CodeMinds</i></h1>
+                    <p class="text-clr5">Jasa Pembuatan Website dan Aplikasi</p>
+                    <hr class="text-clr5" style="width:100px;">
+                    <p class="text-clr5 text-justify">CodeMinds adalah tim profesional yang menyediakan layanan unggulan dalam pengembangan website, aplikasi, dan konsultasi IT. Kami juga berpengalaman dalam desain UI/UX untuk memastikan tampilan dan pengalaman pengguna yang optimal. Dengan pendekatan kolaboratif dan inovatif, kami siap membantu Anda mewujudkan ide menjadi solusi digital yang fungsional dan estetis.</p>
+                    <a href="<?= base_url('') ?>" class="btn btn-clr4 mt-3"><i class="fas fa-phone me-2"></i> konsultasi dulu aja</a>
+                </div>
             </div>
         </div>
-        <div class="col-md-6 row m-0 p-0 d-flex justify-content-center align-items-center">
-            <div class="col-10 col-md-7 d-flex justify-content-center align-items-center mx-5 px-5 mt-5 pt-5 mt-md-5 pt-md-5">
+        <div class="col-md-5 row m-0 p-0 d-flex justify-content-center align-items-center">
+            <div class="col-10 col-lg-10 col-xl-8 d-flex justify-content-center align-items-center mx-5 px-5 mt-5 pt-5 pt-md-5 mt-md-5 mb-5 mb-md-0">
                 <img src="<?= base_url('images/website/il-laptop.png') ?>" class="w-100">
             </div>
         </div>
-        <div class="p-0 d-flex justify-content-center justify-content-md-end pe-0 pe-md-5">
-            <div class="mb-5">
+        <div class="mt-auto p-0 d-flex justify-content-center justify-content-md-end pe-0 pe-md-5" style="height:50px;">
+            <div class="" style="height:40px;">
                 <p class="d-inline m-0 p-0 text-clr5">Find me on</p>
                 <a href="" class="btn btn rounded-circle he-35 we-35 fsz-14 text-clr5 ms-3" style="background-color:#151225;"><i class="fa-brands fa-facebook-f text-clr4"></i></a>
                 <a href="" class="btn btn rounded-circle he-35 we-35 fsz-14 text-clr5 ms-3" style="background-color:#151225;"><i class="fa-brands fa-instagram text-clr4"></i></a>
@@ -28,7 +30,7 @@
     </div>
 </section>
 
-<section class="bg-clr1 py-5" style="min-height:100vh;">
+<section class="bg-clr1 py-5" style="">
     <h5 class="text-center text-secondary mt-5 mb-4">Apa yang kami tawarkan?</h5>
     <div class="row m-0 p-0 justify-content-center mb-5">
         <div class="col-10 col-md-6">
@@ -311,28 +313,114 @@
     </div>
 </section>
 
-<section class="bg-clr2 py-5 overflow-hidden" style="min-height:100vh;">
-    <h5 class="text-secondary text-center">Testimoni Client</h5>
-    <h3 class="text-clr5 text-center mb-5">Apa Kata Mereka tentang Kami?</h3>
-    <div class="my-5">.</div>
-    <div class="mt-5">.</div>
-    <div class="w-100 bg-clr1 mt-5 row m-0 p-0 justify-content-center" style="min-height:200px;">
-        <div class="col-md-10 row m-0 p-0 position-relative">
-            <i class="fas fa-angle-left text-clr5 cursor-pointer position-absolute" style="transform: translate(-50%, -50%);top:50%;right:-60%;"></i>
-            <i class="fas fa-angle-right text-clr5 cursor-pointer position-absolute" style="transform: translate(-50%, -50%);top:50%;left:100%;"></i>
-            <div class="col-md-4 m-0 p-0 position-relative">
-                <img src="<?= base_url('images/testimoni/1.png') ?>" alt="">
-            </div>
-            <div class="col-md-8 m-0 p-5 d-flex align-items-center">
-                <div class="">
-                    <p class="text-secondary">CodeMinds adalah desainer terbaik yang pernah bekerja sama dengan saya. Desain UI yang ia buat sangat bagus dan sistem desain yang ia integrasikan memungkinkan perbaikan langsung di setiap area aplikasi. Saya tidak sabar untuk bekerja sama dengannya di proyek-proyek mendatang.</p>
-                    <h4 class="text-clr4">Iqbal Hafish</h4>
-                    <p class="text-clr5">Project Manager, DaurUang</p>
+<section class="bg-clr2 py-5 overflow-hidden">
+    <div class="mb-5">
+        <h5 class="text-secondary text-center">Testimoni Client</h5>
+        <h3 class="text-clr5 text-center mb-5">Apa Kata Mereka tentang Kami?</h3>
+        <div class="my-5">.</div>
+        <div class="w-100 bg-clr1 mt-5 row m-0 p-0 justify-content-center" style="min-height:200px;">
+            <div class="col-md-10 row m-0 p-0 position-relative">
+                <!-- <i class="fas fa-angle-left text-clr5 cursor-pointer position-absolute" style="transform: translate(-50%, -50%);top:50%;right:-47%;"></i>
+                <i class="fas fa-angle-right text-clr5 cursor-pointer position-absolute" style="transform: translate(-50%, -50%);top:50%;left:150%;"></i> -->
+                <div class="col-md-5 row m-0 p-0 position-relative d-flex justify-content-center align-items-center">
+                    <div class="d-flex justify-content-center px-3 mb-0 mb-5 mb-lg-0 position-relative" style="transform: translate(-50%, -50%);left:45%;top:30%;width:90%">
+                        <div class="">
+                            <img src="<?= base_url('images/testimoni/1.png') ?>" class="w-100 shadow">
+                            <div class="d-flex justify-content-center gap-1 mt-4">
+                                <div class="card m-0 p-0 bg-secondary border-none" style="height:7px;width:35px;" id="tawarkan-1-list"></div>
+                                <div class="card m-0 p-0 bg-clr4 border-none" style="height:7px;width:35px;" id="tawarkan-2-list"></div>
+                                <div class="card m-0 p-0 bg-secondary border-none" style="height:7px;width:35px;" id="tawarkan-3-list"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7 m-0 p-5 d-flex align-items-center">
+                    <div class="">
+                        <p class="text-secondary">CodeMinds adalah desainer terbaik yang pernah bekerja sama dengan saya. Desain UI yang ia buat sangat bagus dan sistem desain yang ia integrasikan memungkinkan perbaikan langsung di setiap area aplikasi. Saya tidak sabar untuk bekerja sama dengannya di proyek-proyek mendatang.</p>
+                        <h4 class="text-clr4">Iqbal Hafish</h4>
+                        <p class="text-clr5">Project Manager, DaurUang</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
-<section class="bg-clr2 pb-5 overflow-hidden" style="min-height:100vh;">
+<section class="bg-clr2 pb-5 overflow-hidden">
+    <div class="mb-5">
+        <div class="d-flex justify-content-center position-relative my-5">
+            <img src="<?= base_url('images/elemen/text-tools.png') ?>" style="width:100%;">
+            <h3 class="text-center position-absolute text-clr5 fw-bold" style="transform:translate(-50%,-50%);top:60%;left:50%;">Spesial Tools Kami</h3>
+        </div>
+        <div class="">.</div>
+        <div class="row m-0 p-0 justify-content-center">
+            <div class="col-6 col-md-4 col-lg-2 m-0 p-0 mb-5 pb-4 d-flex justify-content-center align-items-center">
+                <img src="<?= base_url('images/tools/1.png') ?>" style="width:70%;">
+            </div>
+            <div class="col-6 col-md-4 col-lg-2 m-0 p-0 mb-5 pb-4 d-flex justify-content-center align-items-center">
+                <img src="<?= base_url('images/tools/2.png') ?>" style="width:70%;">
+            </div>
+            <div class="col-6 col-md-4 col-lg-2 m-0 p-0 mb-5 pb-4 d-flex justify-content-center align-items-center">
+                <img src="<?= base_url('images/tools/3.png') ?>" style="width:70%;">
+            </div>
+            <div class="col-6 col-md-4 col-lg-2 m-0 p-0 mb-5 pb-4 d-flex justify-content-center align-items-center">
+                <img src="<?= base_url('images/tools/4.png') ?>" style="width:70%;">
+            </div>
+            <div class="col-6 col-md-4 col-lg-2 m-0 p-0 mb-5 pb-4 d-flex justify-content-center align-items-center">
+                <img src="<?= base_url('images/tools/5.png') ?>" style="width:70%;">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-clr1 py-5 overflow-hidden">
+    <div class="row m-0 p-0 py-5">
+        <div class="col-md-8 m-0 p-0 d-flex align-items-center justify-content-center">
+            <div class="mx-5">
+                <h4 class="text-clr5">Hubungi Kami</h4>
+                <p class="text-secondary mt-4">Untuk pertanyaan bisnis dan kemitraan, silakan hubungi kami di bawah ini!</p>
+                <div class="mt-4">
+                    <!--  -->
+                    <div class="row m-0 p-0">
+                        <div class="col-1 m-0 p-0 d-flex justify-content-center align-items-center">
+                            <img src="<?= base_url('images/icon/circle-phone.png') ?>" style="width:90%">
+                        </div>
+                        <div class="col-9 m-0 p-0 d-flex justify-content-start align-items-center">
+                            <div class="ms-4">
+                                <p class="text-secondary m-0">No Handphone</p>
+                                <p class="text-clr5 m-0">0899-3415-875</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--  -->
+                    <div class="row m-0 p-0 mt-4">
+                        <div class="col-1 m-0 p-0 d-flex justify-content-center align-items-center">
+                            <img src="<?= base_url('images/icon/circle-mail.png') ?>" style="width:90%">
+                        </div>
+                        <div class="col-9 m-0 p-0 d-flex justify-content-start align-items-center">
+                            <div class="ms-4">
+                                <p class="text-secondary m-0">Email</p>
+                                <p class="text-clr5 m-0">codemindssoftware@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--  -->
+                    <div class="row m-0 p-0 mt-4">
+                        <div class="col-1 m-0 p-0 d-flex justify-content-center align-items-center">
+                            <img src="<?= base_url('images/icon/circle-pin.png') ?>" style="width:90%">
+                        </div>
+                        <div class="col-9 m-0 p-0 d-flex justify-content-start align-items-center">
+                            <div class="ms-4">
+                                <p class="text-secondary m-0">Alamat</p>
+                                <p class="text-clr5 m-0">Pondok Alam Sigura Indah, Sigura gura Malang</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 m-0 p-0 pt-5 pt-md-0 d-flex justify-content-center align-items-center">
+            <img src="<?= base_url('images/elemen/decoration-footer.png') ?>" style="width:50%;" class="mt-5 mt-md-0">
+        </div>
+    </div>
 </section>
--- app/Views/templates/navbar-guest.php
+++ app/Views/templates/navbar-guest.php
@@ -1,25 +1,25 @@
 <nav class="navbar navbar-expand-lg bg-clr2 w-100 position-absolute" style="height:90px;z-index:10;">
   <div class="container-fluid px-3 px-md-5">
-    <a class="navbar-brand text-clr5 ls-s d-flex align-items-center" href="#" style="height:90px;">Code<i class="text-clr4 fst-normal">Minds</i></a>
+    <a class="navbar-brand text-clr5 ls-s d-flex align-items-center ms-4 ms-lg-0" href="#" style="height:90px;">Code<i class="text-clr4 fst-normal">Minds</i></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon bg-clr4 rounded"></span>
     </button>
     <div class="collapse navbar-collapse bg-clr2" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item d-flex align-items-center">
-          <a class="nav-link text-clr5" href="#">Home</a>
+          <a class="nav-link text-clr5 ms-2 ms-lg-0" href="#">Home</a>
         </li>
         <li class="nav-item d-flex align-items-center">
-          <a class="nav-link text-clr5" href="#">About</a>
+          <a class="nav-link text-clr5 ms-2 ms-lg-0" href="#">About</a>
         </li>
         <li class="nav-item d-flex align-items-center">
-          <a class="nav-link text-clr5" href="#">Project</a>
+          <a class="nav-link text-clr5 ms-2 ms-lg-0" href="#">Project</a>
         </li>
         <li class="nav-item d-flex align-items-center">
-          <a class="nav-link text-clr5" href="#">Blog</a>
+          <a class="nav-link text-clr5 ms-2 ms-lg-0" href="#">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-clr4 border-clr4 rounded ms-0 ms-md-5 p-2 mt-3 mt-md-0" href="#">Contact Me</a>
+          <a class="nav-link text-clr4 border-clr4 rounded ms-0 ms-lg-5 p-2 mt-4 mt-lg-0" href="#">Contact Me</a>
         </li>
       </ul>
     </div>
